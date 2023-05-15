49191abdfd64caa818245088bf1b7790ca6735b2
theRohan2001 <rohanwadhwa2001@gmail.com>
2023-05-15 14:43:40 +0530

    add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -14,13 +14,16 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Lift Workout App</title>
         <meta name="Description" content="This is a home page of my website!" />
-        <meta name="title" property="og:title" content="Lift Workout App" />
+        <meta name="title" property="og:title" content="GymRoutines" />
         <meta property="og:type" content="Website" />
-        <meta name="description" property="og:description" content="Lift Workout App" />
+        <meta name="description" property="og:description" content="Workout Tracking App" />
         <meta name="author" content="Rohan Wadhwa" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
-        <link rel="Lift Icon" type="image/ico" href="/dumbell.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="public/favicon/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="public/favicon/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="public/favicon/favicon-16x16.png"/>
+        <link rel="manifest" href="public/favicon/site.webmanifest"/>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <AuthProvider>
